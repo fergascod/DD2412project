5f179177d9f2cbc727f5e69243846c463c423529
Lorenzo Mazza <lorenzo2.mazza@mail.polimi.it>
2021-12-06 05:25:35 +0100

    distributed version on multiple gpus (first try)

---
+++ distributed_main.py
@@ -0,0 +1,249 @@
+import WRN
+import json
+import os
+import sys
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ.pop('TF_CONFIG', None)
+import tensorflow as tf
+from utils import *
+import os
+import pickle
+import time
+from absl import logging
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+AUTO = tf.data.AUTOTUNE
+RUN_ID = '0002'
+SECTION = 'Cifar10'
+PARENT_FOLDER = os.getcwd()
+
+
+def load_CIFAR_10(tr_batch_size, test_batch_size):
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    # training on a few examples because it's too slow otherwise, you can remove the [] to train on the full dataset
+    training_data = (tf.data.Dataset.from_tensor_slices((x_train[:1024], y_train[:1024]))
+                     .batch(tr_batch_size, drop_remainder=True).prefetch(AUTO)
+                     .shuffle(tr_batch_size * 100000))
+
+    test_data = (tf.data.Dataset.from_tensor_slices((x_test[:1024], y_test[:1024]))
+                 .batch(test_batch_size, drop_remainder=True).prefetch(AUTO)
+                 .shuffle(test_batch_size * 100000))
+
+    classes = tf.unique(tf.reshape(y_train, shape=(-1,)))[0].get_shape().as_list()[0]
+    training_size = x_train.shape[0]
+    test_size = x_test.shape[0]
+    input_dim = training_data.element_spec[0].shape[1:]
+    return training_data, test_data, classes, training_size, test_size, input_dim
+
+
+def main():
+    RUN_FOLDER = 'run/{}/'.format(SECTION)
+    RUN_FOLDER += '_'.join(RUN_ID)
+    if not os.path.exists(RUN_FOLDER):
+        os.makedirs(RUN_FOLDER)
+        os.mkdir(os.path.join(RUN_FOLDER, 'weights'))
+        os.mkdir(os.path.join(RUN_FOLDER, 'metrics'))
+
+    physical_devices = tf.config.list_physical_devices('GPU')
+    for device in physical_devices:
+        tf.config.experimental.set_memory_growth(device, True)
+
+    batch_repetitions = 1
+    BATCH_SIZE = 512
+    strategy = tf.distribute.MirroredStrategy()
+    num_workers = 2
+    per_core_batch_size = int(BATCH_SIZE / num_workers)
+    train_batch_size = int(per_core_batch_size / batch_repetitions)
+    test_batch_size= int(per_core_batch_size)
+
+    # Number of subnetworks (baseline=3)
+    M = 3
+    tr_data, test_data, classes,\
+    train_dataset_size, test_dataset_size, input_shape = load_CIFAR_10(train_batch_size,test_batch_size)
+    tr_data = strategy.experimental_distribute_dataset(tr_data)
+    test_data = strategy.experimental_distribute_dataset(test_data)
+    # WRN params
+    n, k = 28, 10
+
+    lr_decay_ratio = 0.1
+    base_lr = 0.1 * train_batch_size / 128
+    lr_warmup_epochs = 1
+    lr_decay_epochs = [80, 160, 180]
+    EPOCHS = 250
+    l2_reg = 3e-4
+    steps_per_epoch = train_dataset_size // train_batch_size
+    steps_per_eval = test_dataset_size // test_batch_size
+
+    with strategy.scope():
+        lr_schedule = WarmUpPiecewiseConstantSchedule(
+            steps_per_epoch,
+            base_lr,
+            decay_ratio=lr_decay_ratio,
+            decay_epochs=lr_decay_epochs,
+            warmup_epochs=lr_warmup_epochs)
+        optimizer = tf.keras.optimizers.SGD(
+            lr_schedule, momentum=0.9, nesterov=True)
+
+        training_metrics = {
+            'train/negative_log_likelihood': tf.keras.metrics.Mean(),
+            'train/accuracy': tf.keras.metrics.CategoricalAccuracy(),
+            'train/loss': tf.keras.metrics.Mean(),
+            'train/ece': ExpectedCalibrationError(),
+        }
+
+        test_metrics = {
+            'test/negative_log_likelihood': tf.keras.metrics.Mean(),
+            'test/accuracy': tf.keras.metrics.CategoricalAccuracy(),
+            'test/ece': ExpectedCalibrationError(),
+        }
+
+        model = WRN.build_model(input_dims=[M] +input_shape,
+                                output_dim=classes,
+                                n=n,
+                                k=k,
+                                M=M)
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=os.path.join(RUN_FOLDER, 'metrics/logs')
+                                                              , update_freq='epoch')
+        tensorboard_callback.set_model(model)
+        print(model.summary())
+        train_metrics_evolution = []
+        test_metrics_evolution = []
+        checkpoint = tf.train.Checkpoint(model=model, optimizer=optimizer)
+        latest_checkpoint = tf.train.latest_checkpoint(RUN_FOLDER)
+        initial_epoch = 0
+        if latest_checkpoint:
+            checkpoint.restore(latest_checkpoint)
+            logging.info('Loaded checkpoint %s', latest_checkpoint)
+            initial_epoch = optimizer.iterations.numpy() // steps_per_epoch
+
+    @tf.function
+    def train_step(iterator):
+        """Training step function."""
+
+        def step_fn(inputs):
+            """Per-Replica step function."""
+
+            images = inputs[0]
+            labels = tf.squeeze(tf.one_hot(inputs[1], 10), axis=-2)
+            BATCH_SIZE = tf.shape(images)[0]
+
+            main_shuffle = tf.random.shuffle(tf.tile(
+                tf.range(BATCH_SIZE), [batch_repetitions]))
+            to_shuffle = tf.cast(tf.cast(tf.shape(main_shuffle)[0], tf.float32),tf.int32)
+            shuffle_indices = [
+                tf.concat([tf.random.shuffle(main_shuffle[:to_shuffle]),
+                           main_shuffle[to_shuffle:]], axis=0)
+                for _ in range(M)]
+            images = tf.stack([tf.gather(images, indices, axis=0)
+                               for indices in shuffle_indices], axis=1)
+            labels = tf.stack([tf.gather(labels, indices, axis=0)
+                               for indices in shuffle_indices], axis=1)
+
+            with tf.GradientTape() as tape:
+                logits = model(images, training=True)
+                negative_log_likelihood = tf.reduce_mean(tf.reduce_sum(
+                    tf.keras.losses.categorical_crossentropy(
+                        labels, logits, from_logits=True), axis=1))
+                filtered_variables = []
+                for var in model.trainable_variables:
+                    if ('kernel' in var.name or 'batch_norm' in var.name or
+                            'bias' in var.name):
+                        filtered_variables.append(tf.reshape(var, (-1,)))
+                l2_loss = l2_reg * 2 * tf.nn.l2_loss(tf.concat(filtered_variables, axis=0))
+                # tf.nn returns l2 loss divided by 0.5 so we need to double it
+                loss = l2_loss + negative_log_likelihood
+                scaled_loss = loss / strategy.num_replicas_in_sync
+
+            grads = tape.gradient(scaled_loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
+            probabilities = tf.nn.softmax(tf.reshape(logits, [-1, classes]))
+            training_metrics['train/ece'].update_state(tf.argmax(tf.reshape(labels, [-1, classes]), axis=-1)
+                                              , probabilities)
+            training_metrics['train/loss'].update_state(loss)
+            training_metrics['train/negative_log_likelihood'].update_state(negative_log_likelihood)
+            training_metrics['train/accuracy'].update_state(tf.reshape(labels, probabilities.shape), probabilities)
+
+        strategy.run(step_fn, args=(next(iterator),))
+
+
+    @tf.function
+    def test_step(iterator):
+        """Evaluation StepFn."""
+
+        def step_fn(inputs):
+            """Per-Replica StepFn."""
+            images = inputs[0]
+            images = tf.tile(
+                tf.expand_dims(images, 1), [1, M, 1, 1, 1])
+            labels = tf.squeeze(tf.one_hot(inputs[1], 10))
+            logits = model(images, training=False)
+            logits = tf.squeeze(logits)
+            probabilities = tf.nn.softmax(logits)
+            if M > 1:
+                labels_tiled = tf.tile(
+                    tf.expand_dims(labels, 1), [1, M, 1])
+                log_likelihoods = -tf.keras.losses.categorical_crossentropy(
+                    labels_tiled, logits, from_logits=True)
+                negative_log_likelihood = tf.reduce_mean(
+                    -tf.reduce_logsumexp(log_likelihoods, axis=[1]) +
+                    tf.math.log(float(M)))
+                probabilities = tf.math.reduce_mean(probabilities, axis=1)  # marginalize
+            else:
+                negative_log_likelihood = tf.reduce_mean(
+                    tf.keras.losses.categorical_crossentropy(labels, logits, from_logits=True))
+
+            test_metrics['test/ece'].update_state(tf.argmax(tf.reshape(labels, [-1, classes]), axis=-1)
+                                                  , probabilities)
+            # test_ metrics['test/loss'].update_state(loss)
+            test_metrics['test/negative_log_likelihood'].update_state(negative_log_likelihood)
+            test_metrics['test/accuracy'].update_state(tf.reshape(labels, probabilities.shape), probabilities)
+
+        strategy.run(step_fn, args=(next(iterator),))
+
+
+    train_iterator = iter(tr_data)
+    test_iterator = iter(test_data)
+
+    for epoch in range(initial_epoch, EPOCHS):
+        logging.info("Epoch: {}".format(epoch))
+        t1 = time.time()
+        for step in range(steps_per_epoch):
+            train_step(train_iterator)
+            current_step = epoch * steps_per_epoch + (step + 1)
+            max_steps = steps_per_epoch * EPOCHS
+        t2 = time.time()
+        if (epoch + 1) % 50 == 0:
+            checkpoint.save(
+                os.path.join(RUN_FOLDER, 'checkpoint'))
+            model.save_weights(os.path.join(RUN_FOLDER, 'weights/weights_%d.h5' % epoch))
+        train_metric = {}
+        for name, metric in training_metrics.items():
+            train_metric[name] = metric.result().numpy()
+            print("{} : {}".format(name, metric.result().numpy()))
+            metric.reset_states()
+        train_metrics_evolution.append(train_metric)
+
+
+        t3 = time.time()
+        for step in range(steps_per_eval):
+            test_step(test_iterator)
+        t4 = time.time()
+        test_metric = {}
+        for name, metric in test_metrics.items():
+            test_metric[name] = metric.result().numpy()
+            print("{} : {}".format(name, metric.result().numpy()))
+            metric.reset_states()
+        test_metrics_evolution.append(test_metric)
+        print(f"Epoch took {t4 - t1}s. Training took {t2 - t1} s and testing {t4 - t3} s\n")
+
+    model.save_weights(os.path.join(RUN_FOLDER, 'weights/final_weights.h5'))
+    final_checkpoint_name = checkpoint.save(
+    os.path.join(RUN_FOLDER, 'checkpoint'))
+    logging.info('Saved last checkpoint to %s', final_checkpoint_name)
+    metrics_evo = (train_metrics_evolution, test_metrics_evolution)
+    with open(os.path.join(RUN_FOLDER, 'metrics/metrics_evo.pickle'), 'wb') as f:
+        pickle.dump(metrics_evo, f)
+
+if __name__ == '__main__':
+    main()
+
