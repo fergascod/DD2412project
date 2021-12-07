19a548ac078a075003d3aa2537957c1e06b8a5a3
fergascod <fergascod@gmail.com>
2021-12-08 00:33:30 +0100

    Added 3D plot and titles

--- independence.py
+++ independence.py
@@ -5,6 +5,7 @@ import os
 import random
 import time
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 # from keras import backend as K
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -30,7 +31,7 @@ def plot_variance_activations(model, layer_name, x_train):
     ex1, ex2 = random.sample(x_train, 2)
     act_1, act_2, act_3 = [], [], []
 
-    for _ in range(3):
+    for _ in range(10):
         ex3 = random.sample(x_train, 1)[0]
         example1 = [tf.convert_to_tensor(ex3),
                     tf.convert_to_tensor(ex1),
@@ -53,10 +54,21 @@ def plot_variance_activations(model, layer_name, x_train):
     act_tfs = [tf.stack(act) for act in [act_1, act_2, act_3]]
     var_1, var_2, var_3 = [tf.math.reduce_std(act_tf, axis=0) for act_tf in act_tfs]
     print(var_1, var_2, var_3)
-    plt.scatter(var_1, var_2)
+    plt.scatter(var_1, var_2, s=1)
     plt.title("Variance of the activation of the neurons in the NN")
     plt.xlabel("Variance when changing input 1")
-    plt.xlabel("Variance when changing input 2")
+    plt.ylabel("Variance when changing input 2")
+    plt.savefig('Activation_Variance.png')
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(var_1, var_2, var_3, s=1)
+    plt.title("Variance of the activation of the neurons in the NN")
+    ax.set_xlabel('Variance changing input 1')
+    ax.set_ylabel('Variance changing input 2')
+    ax.set_zlabel('Variance changing input 3')
+    plt.savefig('Activation_Variance_3D.png')
     plt.show()
 
 plot_variance_activations(model, layer_name, x_train)
