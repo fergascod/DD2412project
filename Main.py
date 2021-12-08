4f7587f4bbf517ed68c11dc62d16e54cf185f49d
Lorenzo Mazza <lorenzo2.mazza@mail.polimi.it>
2021-12-08 04:59:59 +0100

    fix ece error - one-hot encoding labels not decoded

--- Main.py
+++ Main.py
@@ -64,7 +64,7 @@ def train(tr_dataset, model, optimizer, metrics, num_labels):
             optimizer.apply_gradients(zip(grads, model.trainable_variables))
             probabilities = tf.nn.softmax(tf.reshape(logits, [-1, num_labels]))
             flat_labels = tf.reshape(labels, [-1, num_labels])
-            metrics['train/ece'].update_state(flat_labels, probabilities)
+            metrics['train/ece'].update_state(tf.argmax(flat_labels,axis=-1), probabilities)
             metrics['train/loss'].update_state(loss)
             metrics['train/negative_log_likelihood'].update_state(negative_log_likelihood)
             metrics['train/accuracy'].update_state(flat_labels, probabilities)
